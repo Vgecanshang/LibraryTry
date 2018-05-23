5936b10a5658fbe4673d4efb97e7e4fe60f03800
Cyong <30817252+Vgecanshang@users.noreply.github.com>
2018-05-23 17:34:20 +0800

    新增类

---
+++ app/src/main/java/test/cy/com/librarytry/TestA.java
@@ -0,0 +1,12 @@
+package test.cy.com.librarytry;
+
+/**
+ * Created by cy on 2018/5/23.
+ */
+public class TestA {
+
+    public static String sayHello(){
+        return "Test A Hello.";
+    }
+
+}
---
+++ mylibrary/src/main/java/test/cy/com/mylibrary/TestB.java
@@ -0,0 +1,10 @@
+package test.cy.com.mylibrary;
+
+/**
+ * Created by cy on 2018/5/23.
+ */
+public class TestB {
+    public static String sayHello(){
+        return "Test B Hello.";
+    }
+}
